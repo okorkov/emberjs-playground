bcdbffb209aec802290c79d63a8f1a3167a6dd18
okorkov <alex.okorkov@gmail.com>
2021-09-27 20:36:40 -0700

    generated routes, models, templates for movies and characters

---
+++ app/models/character.js
@@ -0,0 +1,16 @@
+// import Model from '@ember-data/model';
+
+// export default class CharacterModel extends Model {}
+
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr(),
+  height: DS.attr(),
+  mass: DS.attr(),
+  hair_color: DS.attr(),
+  skin_color: DS.attr(),
+  eye_color: DS.attr(),
+  birth_year: DS.attr(),
+  gender: DS.attr(),
+});
---
+++ app/models/movie.js
@@ -0,0 +1,16 @@
+// import Model from '@ember-data/model';
+
+// export default class MovieModel extends Model {
+
+// }
+
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  episode_id: DS.attr(),
+  opening_crawl: DS.attr(),
+  director: DS.attr(),
+  producer: DS.attr(),
+  release_date: DS.attr(),
+});
